deeee8afbf4d1a3155a64c978e7a0e849b0cad97
Dawid Domaradzki <dawid-kun@wp.pl>
2021-05-02 15:42:43 +0200

    jquery dropdown menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,18 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
+
+$(document).ready(function () {
+  $(".click").click(function (event) {
+    event.stopPropagation();
+    $(".showup").slideToggle("fast");
+  });
+  $(".showup").on("click", function (event) {
+    event.stopPropagation();
+  });
+});
+
+$(document).on("click", function () {
+  $(".showup").hide();
+});
